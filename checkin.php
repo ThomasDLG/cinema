152b87a91514247c7084957d8f790547f6dd589e
ThomasDLG <thomas.delage02@gmail.com>
2022-09-02 16:20:08 +0200

    add checkin page

--- checkin.php
+++ checkin.php
@@ -3,6 +3,51 @@ $title = 'Acceuil';
 include("inc/header.php");
 ?>
 
+<section id="checkout" class="section-64">
+    <div class="container">
+        <h2 class="text-dark">Réservation confirmée</h2>
+        <p>Lorem ipsum dolor sit.</p>
+        <div class="d-flex row wrap justify-between wrapper">
+
+            <div class="card-xs my-2 mx-2 relative">
+                <a href="">
+                    <img src="src/tickets.png" alt="">
+                </a>
+            </div>
+
+            <div class="card-xs my-2 mx-2 relative">
+                <a href="">
+                    <img src="src/tickets.png" alt="">
+                </a>
+            </div>
+
+            <div class="card-xs my-2 mx-2 relative">
+                <a href="">
+                    <img src="src/food/popcorn.png" alt="">
+                </a>
+            </div>
+
+            <div class="card-xs my-2 mx-2 relative">
+                <a href="">
+                    <img src="src/food/doritos.png" alt="">
+                </a>
+            </div>
+
+            <div class="card-xs my-2 mx-2 relative">
+                <a href="">
+                    <img src="src/food/cocacola.png" alt="">
+                </a>
+            </div>
+
+            <div class="card-xs my-2 mx-2 relative">
+                <a href="">
+                    <img src="src/food/cocacola.png" alt="">
+                </a>
+            </div>
+
+        </div>
+    </div>
+</section>
 
 <?php
 include("inc/footer.php")
