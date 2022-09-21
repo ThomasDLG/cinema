e45ffad79c2e6feb784601e7ac206ae98411748a
ThomasDLG <thomas.delage02@gmail.com>
2022-09-21 16:15:51 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,47 +1,37 @@
 // ACCORDION
 
 const items = document.querySelectorAll(".accordion button");
+if (items) {
+  function toggleAccordion() {
+    const itemToggle = this.getAttribute("aria-expanded");
 
-function toggleAccordion() {
-  const itemToggle = this.getAttribute("aria-expanded");
+    for (i = 0; i < items.length; i++) {
+      items[i].setAttribute("aria-expanded", "false");
+    }
 
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute("aria-expanded", "false");
+    if (itemToggle == "false") {
+      this.setAttribute("aria-expanded", "true");
+    }
   }
 
-  if (itemToggle == "false") {
-    this.setAttribute("aria-expanded", "true");
-  }
+  items.forEach((item) => item.addEventListener("click", toggleAccordion));
 }
-
-items.forEach((item) => item.addEventListener("click", toggleAccordion));
-
-// RADIO DATE BUTTON
-
-// let radioDate = document.querySelectorAll(".radio-date input[type=radio]");
-// let radioText = document.querySelector(".radio-text");
-
-// radioDate.array.forEach(button => {
-//   if (button.checked) {
-//     radioText.classList.add("text-light")
-//   } else {
-//     radioText.classList.add("text-dark")
-//   }
-// });
 
 // SEARCHING POPUP
 
 let search = document.getElementById("searching");
-let popup = document.querySelector(".searching-popup");
-let closed = document.querySelector(".close-searching-popup");
+if (search) {
+  let popup = document.querySelector(".searching-popup");
+  let closed = document.querySelector(".close-searching-popup");
 
-search.addEventListener("click", function (evt) {
-  popup.classList.add("search-open");
-});
+  search.addEventListener("click", function (evt) {
+    popup.classList.add("search-open");
+  });
 
-closed.addEventListener("click", function (evt) {
-  popup.classList.remove("search-open");
-});
+  closed.addEventListener("click", function (evt) {
+    popup.classList.remove("search-open");
+  });
+}
 
 // PLUS MENU
 
@@ -57,13 +47,12 @@ plusIcon.addEventListener("click", function (evt) {
 // let selected = (document.getElementById(".select__trigger span").innerHTML =
 //   "Filtrer par genres");
 let select = document.querySelector(".select");
-let optionContainer = document.querySelector(".option-container");
-let option = document.querySelector(".option-select");
+if (select) {
+  let optionContainer = document.querySelector(".option-container");
+  let option = document.querySelector(".option-select");
 
-select.addEventListener("click", function () {
-  console.log("click");
-  optionContainer.classList.toggle("option-active");
-});
-
-
-
+  select.addEventListener("click", function () {
+    console.log("click");
+    optionContainer.classList.toggle("option-active");
+  });
+}
--- mytoile.php
+++ mytoile.php
@@ -7,25 +7,36 @@ include("inc/header.php");
 <section id="about" class="section-64">
     <div class="container">
         <h4 class="text-dark pb-2">My-Toile</h4>
-        <p class="text-dark pb-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, quia.</p>
+        <p class="text-dark pb-2">Profitez du cinéma à moindre coût et accédez à une multitude d’avantages intéressants !</p>
         <h4 class="text-dark pb-2">Avantages</h4>
         <div class="accordion">
                 <div class="accordion-item">
                   <button id="accordion-button-1" aria-expanded="false"><span class="accordion-title">Billet premium My-Toile</span><span class="icon" aria-hidden="true"></span></button>
                   <div class="accordion-content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                    <p>Vous pouvez découvrir la joie du cinéma à moindre coût en tant que membre My-toile.
+                        Les billet premiums My-toile sont disponibles pour les membres uniquement sur certaines séances du mardi au dimanche et toute la journée le lundi des membres, avec des prix à partir de 6 €.
+                    </p>
                   </div>
                 </div>
                 <div class="accordion-item">
                   <button id="accordion-button-2" aria-expanded="false"><span class="accordion-title">Réservation rapide</span><span class="icon" aria-hidden="true"></span></button>
                   <div class="accordion-content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                    <p>Économisez du temps et des efforts lors de la réservation de billet premiums en ligne.
+                        Connectez-vous à votre compte My-toile et votre nom et votre adresse seront déjà saisis. Vous n’aurez donc pas à le faire à chaque réservation.
+                    </p>
                   </div>
                 </div>
                 <div class="accordion-item">
                   <button id="accordion-button-3" aria-expanded="false"><span class="accordion-title">Offres exclusives</span><span class="icon" aria-hidden="true"></span></button>
                   <div class="accordion-content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                    <p>Rejoignez My-toile gratuitement et obtenez plus
+                        En tant que membre My-toile, vous pouvez accéder à une multitude d’avantages intéressants :
+                        Payez moins avec des billet premiums My-toile sur certaines séances du mardi au dimanche et toute la journée le lundi des membres
+                        Une expérience de réservation plus rapide et meilleure
+                        Recevez des offres personnalisées via l’application My-toile
+                        Remboursements et modifications de réservation en libre-service
+                        Des recommandations de films personnalisées pour ne jamais manquer un film à ne pas manquer
+                      </p>
                   </div>
                 </div>
               </div>
