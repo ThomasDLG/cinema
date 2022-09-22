6da72b9833dcf214d654299fe706a5dc89e47323
ThomasDLG <thomas.delage02@gmail.com>
2022-09-22 08:57:44 +0200

    update

--- tarifs.php
+++ tarifs.php
@@ -11,25 +11,34 @@ include("inc/header.php");
                 <div class="accordion-item">
                   <button id="accordion-button-1" aria-expanded="false"><span class="accordion-title">Tickets</span><span class="icon" aria-hidden="true"></span></button>
                   <div class="accordion-content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
-                  </div>
-                </div>
-                <div class="accordion-item">
-                  <button id="accordion-button-2" aria-expanded="false"><span class="accordion-title">Evénements</span><span class="icon" aria-hidden="true"></span></button>
-                  <div class="accordion-content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                    <ul>
+                      <li class="my-2">Normal : 10,80 €</li>
+                      <li class="my-2">Réduit : 8 €</li>
+                      <li class="my-2">-14 ans : 5,50 €</li>
+                      <li class="my-2">My-Toile : 6 €</li>
+                    </ul>
                   </div>
                 </div>
                 <div class="accordion-item">
                   <button id="accordion-button-3" aria-expanded="false"><span class="accordion-title">Séance i-Sense</span><span class="icon" aria-hidden="true"></span></button>
                   <div class="accordion-content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                    <ul>
+                      <li class="my-2">Normal : 19,90 € €</li>
+                      <li class="my-2">Réduit : 12 €</li>
+                      <li class="my-2">-14 ans : 9,50 €</li>
+                      <li class="my-2">My-Toile : 10 €</li>
+                    </ul>
                   </div>
                 </div>
                 <div class="accordion-item">
                   <button id="accordion-button-4" aria-expanded="false"><span class="accordion-title">Luxe Diner</span><span class="icon" aria-hidden="true"></span></button>
                   <div class="accordion-content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                  <ul>
+                      <li class="my-2">Normal : 19,90 € €</li>
+                      <li class="my-2">Réduit : 12 €</li>
+                      <li class="my-2">-14 ans : 9,50 €</li>
+                      <li class="my-2">My-Toile : 10 €</li>
+                    </ul>
                   </div>
                 </div>
                 <div class="accordion-item">
